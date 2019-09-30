f0680da1aee4a197514fa4bcacf0f69aa8416867
Talrem <43709968+Talrem@users.noreply.github.com>
2019-09-30 22:49:08 +0200

    Add files via upload

---
+++ commands/AWPMaster.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+		message.channel.send('', {
+			file : './images/AWPMaster.gif'
+		});
+  return;
+}
+
+module.exports.help = {
+  name: "AWPMaster",
+  type: "fun",
+	usage: "AWPMaster",
+  desc: "je montre qu'Adrien est un AWP Master."
+}
