ee2656a094d17038f48b931bfd8429a1743e42dc
Talrem <43709968+Talrem@users.noreply.github.com>
2019-12-12 01:55:01 +0100

    Update deck.js

--- commands/deck.js
+++ commands/deck.js
@@ -5,7 +5,7 @@ const sefile = require("../decks.json");
 module.exports.run = async (bot, message, args) => {
   if(!args.length) return message.reply("Veuillez préciser un ID.");
   let taille = sefile[-1].number;
-  if(args[0] > taille) return message.channel.send("Le deck n'existe pas.");
+  if(args[0] >= taille) return message.channel.send("Le deck n'existe pas.");
   return message.channel.send(sefile[args[0]].n + " " + sefile[args[0]].p + " " + sefile[args[0]].c + " " + sefile[args[0]].w + " " + sefile[args[0]].t);
 }
 
