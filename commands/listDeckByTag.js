f01e875413ea5b100c88718e72aa5210b47ab5d8
David Merlat <david.merlat28@gmail.com>
2020-06-18 18:08:29 +0200

    Gestion des decks avec listDeckByTag

--- commands/listDeckByTag.js
+++ commands/listDeckByTag.js
@@ -10,11 +10,13 @@ function isIn(tab, argument) {
     return 0;
 }
 
-function whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, argument){
+function whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, argument){
     if(isIn(lesTiers, argument)) return "t";
     if(isIn(lesProvenances, argument)) return "p";
     if(isIn(lesConcepts, argument)) return "c";
     if(isIn(lesWinCons, argument)) return "w";
+    if(isIn(lesNoms, argument)) return "n";
+    if(isIn(lesCreateurs, argument)) return "u";
     return "";
 }
 
@@ -24,11 +26,13 @@ module.exports.run = async (bot, message, args) => {
     if (!(message.member.roles.some(role => role.name === roleName))) {
         return message.reply("Vous n'êtes pas un Dueliste, je ne peux pas vous laisser faire ça.");
     }
-    if(args.length >= 4 && args.length <= 0) return message.reply("La liste des arguments est invalide, vous devez préciser si vous souhaitez <Provenance> <Concept> <WinCon> <Tier> dans l'ordre que vous voulez.");
+    if(args.length >= 6 && args.length <= 0) return message.reply("La liste des arguments est invalide, vous devez préciser si vous souhaitez <Nom> <Créateur> <Provenance> <Concept> <WinCon> <Tier> dans l'ordre que vous voulez.");
     let taille = sefile[-1].number;
     var i = 0;
     var j = 0;
     var k = 0;
+    var uOk = 1;
+    var nOk = 1;
     var tOk = 1;
     var pOk = 1;
     var cOk = 1;
@@ -37,17 +41,24 @@ module.exports.run = async (bot, message, args) => {
     var argument1 = "";
     var argument2 = "";
     var argument3 = "";
+    var argument4 = "";
+    var argument5 = "";
     var lesDeck = new Array();
+    var lesDeckID = new Array();
     var lesProvenances = new Array();
     var lesConcepts = new Array();
     var lesWinCons = new Array();
     var lesTiers = new Array();
+    var lesCreateurs = new Array();
+    var lesNoms = new Array();
     /*Récupération de toutes les variables de tout les decks de l'utilisateur*/
     for(i = 0; i < taille; i++){
         lesProvenances[k] = sefile[i].p;
         lesConcepts[k] = sefile[i].c;
         lesWinCons[k] = sefile[i].w;
         lesTiers[k] = sefile[i].t;
+        lesNoms[k] = sefile[i].n
+        lesCreateurs[k] = sefile[i].u
         k++;
     }
 
@@ -57,15 +68,19 @@ module.exports.run = async (bot, message, args) => {
             lesDeck[j] = sefile[i];
             j++;
         }else{
-            if(args[0]) argument0 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, args[0]);
-            if(args[1]) argument1 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, args[1]);
-            if(args[2]) argument2 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, args[2]);
-            if(args[3]) argument3 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, args[3]);
+            if(args[0]) argument0 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[0]);
+            if(args[1]) argument1 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[1]);
+            if(args[2]) argument2 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[2]);
+            if(args[3]) argument3 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[3]);
+            if(args[4]) argument4 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[4]);
+            if(args[5]) argument5 = whatTab(lesTiers, lesProvenances, lesConcepts, lesWinCons, lesNoms, lesCreateurs, args[5]);
             tOk = 1;
             pOk = 1;
             cOk = 1;
             wOk = 1;
-            if(argument0 != "" || argument1 != "" || argument2 != "" || argument3 != ""){
+            uOk = 1;
+            nOk = 1;
+            if(argument0 != "" || argument1 != "" || argument2 != "" || argument3 != "" || argument4 != "" || argument5 != ""){
                 switch (argument0) {
                     case "t":
                     if(args[0].toLowerCase() != sefile[i].t.toLowerCase()) tOk = 0;
@@ -78,6 +93,12 @@ module.exports.run = async (bot, message, args) => {
                     break;
                     case "w":
                     if(args[0].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
+                    break;
+                    case "n":
+                    if(args[0].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[0].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
                     break;
                     default:break;
                 }
@@ -94,6 +115,12 @@ module.exports.run = async (bot, message, args) => {
                     case "w":
                     if(args[1].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
                     break;
+                    case "n":
+                    if(args[1].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[1].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
+                    break;
                     default:break;
                 }
                 switch (argument2) {
@@ -108,6 +135,12 @@ module.exports.run = async (bot, message, args) => {
                     break;
                     case "w":
                     if(args[2].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
+                    break;
+                    case "n":
+                    if(args[2].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[2].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
                     break;
                     default:break;
                 }
@@ -124,9 +157,58 @@ module.exports.run = async (bot, message, args) => {
                     case "w":
                     if(args[3].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
                     break;
+                    case "n":
+                    if(args[3].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[3].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
+                    break;
                     default:break;
                 }
-                if(tOk && pOk && cOk && wOk){
+                switch (argument4) {
+                    case "t":
+                    if(args[4].toLowerCase() != sefile[i].t.toLowerCase()) tOk = 0;
+                    break;
+                    case "p":
+                    if(args[4].toLowerCase() != sefile[i].p.toLowerCase()) pOk = 0;
+                    break;
+                    case "c":
+                    if(args[4].toLowerCase() != sefile[i].c.toLowerCase()) cOk = 0;
+                    break;
+                    case "w":
+                    if(args[4].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
+                    break;
+                    case "n":
+                    if(args[4].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[4].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
+                    break;
+                    default:break;
+                }
+                switch (argument5) {
+                    case "t":
+                    if(args[5].toLowerCase() != sefile[i].t.toLowerCase()) tOk = 0;
+                    break;
+                    case "p":
+                    if(args[5].toLowerCase() != sefile[i].p.toLowerCase()) pOk = 0;
+                    break;
+                    case "c":
+                    if(args[5].toLowerCase() != sefile[i].c.toLowerCase()) cOk = 0;
+                    break;
+                    case "w":
+                    if(args[5].toLowerCase() != sefile[i].w.toLowerCase()) wOk = 0;
+                    break;
+                    case "n":
+                    if(args[5].toLowerCase() != sefile[i].n.toLowerCase()) wOk = 0;
+                    break;
+                    case "u":
+                    if(args[5].toLowerCase() != sefile[i].u.toLowerCase()) wOk = 0;
+                    break;
+                    default:break;
+                }
+                if(tOk && pOk && cOk && wOk && nOk && uOk){
+                    lesDeckID[j] = i;
                     lesDeck[j] = sefile[i];
                     j++;
                 }
@@ -136,7 +218,7 @@ module.exports.run = async (bot, message, args) => {
     if(j == 0) return message.reply("Aucun deck ne correspond aux arguments fournis...");
     let messageToSend = "";
     for(let k = 0; k < lesDeck.length; k++){
-        messageToSend+= "`"+lesDeck[k].n + "` (`"+lesDeck[k].t+"`) de `" + lesDeck[k].u+"`\n";
+        messageToSend+= lesDeckID[k]+" `"+lesDeck[k].n + "` (`"+lesDeck[k].t+"`) de `" + lesDeck[k].u+"`\n";
         if(k%15==0 && k != 0){
             message.channel.send(messageToSend);
             messageToSend = "";
@@ -150,6 +232,6 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
     name: "listDeckByTag",
     type: "YuGiOh", //social fun Private ou admin
-    usage: "listDeckByTag <Tag> <...> <...> <...>",
+    usage: "listDeckByTag <Tag> <...> <...> <...> <...> <...>",
     desc: "je donne le nom des decks qui correspondent aux arguments donnés. Les tiers valables sont `Ban`, `VeryHigh`, `High`, `Mid`, `Low`, `VeryLow` et `Untiered`."
 }
